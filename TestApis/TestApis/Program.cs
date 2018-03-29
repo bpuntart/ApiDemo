d4e4757f35657ac6c46bf65442dd735b96a77914
bpuntart <b.art@telenet.be>
2018-03-29 15:31:00 +0200

    Add files via upload

---
+++ TestApis/TestApis/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using TestApis.DAL;
+
+namespace TestApis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var dataGetter = new DataGetter();
+
+            IEnumerable<TimeSerie> items = dataGetter.GetWeeklyTimeSeriesForEquitiy(Equity.MSFT);
+        }
+    }
+
+    internal class DataGetter
+    {
+        internal IEnumerable<TimeSerie> GetWeeklyTimeSeriesForEquitiy(Equity equity)
+        {
+            var consumer = new Consumer();
+
+            var urlable = new Urlable()
+            {
+                Protocol = "https",
+                ApiKey = "X45KWL2GJGCXJ8B0",
+                BaseDomain = "www.alphavantage.co/query?",
+                Function = "TIME_SERIES_WEEKLY",
+                Parameters = new Dictionary<string, string>()
+                {
+                     {"symbol", equity.ToString()}
+                }
+            };
+
+            var url = Consumer.urlBuilder(urlable);
+                
+            return Mapper.Map(consumer.Get(url));
+        }
+    }
+
+    internal class Mapper
+    {
+        public static List<TimeSerie> Map(WeeklyPriceSerie get)
+        {
+           return new List<TimeSerie>();
+        }
+    }
+    
+    public interface IUrlable
+    {
+        string Protocol { get; set; }
+        string BaseDomain { get; set; }
+        string Function { get; set; }
+        string ApiKey { get; set; }
+        Dictionary<string, string> Parameters { get; set; }
+    }
+
+    public class Urlable : IUrlable
+    {
+        public string Protocol { get; set; }
+        public string BaseDomain { get; set; }
+        public string Function { get; set; }
+        public string ApiKey { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+
+    public class Equity
+    {
+        private string _typeKeyWord;
+
+        private Equity(string typeKeyWord)
+        {
+            _typeKeyWord = typeKeyWord;
+        }
+
+        public override string ToString()
+        {
+            return _typeKeyWord;
+        }
+
+        public static Equity MSFT = new Equity("MSFT");
+    }
+
+    internal class TimeSerie
+    {
+        
+    }
+}
---
+++ TestApis/TestApis/WeeklyPriceSeries.cs
@@ -0,0 +1,79 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using Series;
+//
+//    var weeklyPriceSerie = WeeklyPriceSerie.FromJson(jsonString);
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TestApis
+{
+    public partial class WeeklyPriceSerie
+    {
+        [JsonProperty("Meta Data")]
+        public MetaData MetaData { get; set; }
+
+        [JsonProperty("Weekly Time Series")]
+        public Dictionary<DateTime, Serie> WeeklyTimeSeries { get; set; }
+    }
+
+    public class Serie
+    {
+        [JsonProperty("1. open")]
+        public decimal Open { get; set; }
+
+        [JsonProperty("2. high")]
+        public decimal High { get; set; }
+
+        [JsonProperty("3. low")]
+        public decimal Low { get; set; }
+
+        [JsonProperty("4. close")]
+        public decimal Close { get; set; }
+
+        [JsonProperty("5. volume")]
+        public long Volume { get; set; }
+
+    }
+
+    public class MetaData
+    {
+        [JsonProperty("1. Information")]
+        public string The1Information { get; set; }
+
+        [JsonProperty("2. Symbol")]
+        public string The2Symbol { get; set; }
+
+        [JsonProperty("3. Last Refreshed")]
+        public System.DateTimeOffset The3LastRefreshed { get; set; }
+
+        [JsonProperty("4. Time Zone")]
+        public string The4TimeZone { get; set; }
+    }
+
+    public partial class WeeklyPriceSerie
+    {
+        public static WeeklyPriceSerie FromJson(string json) => JsonConvert.DeserializeObject<WeeklyPriceSerie>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this WeeklyPriceSerie self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                    new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+                },
+        };
+    }
+}
