2c6db2ffa63c07fee46aead44765b74a62dc9f63
Bart Goossens <Bart.Goossens@delen.be>
2018-04-09 16:39:30 +0200

    change location

---
+++ TestApis/TestApis/Consumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+
+namespace TestApis.DAL
+{
+    public class Consumer
+    {
+        private WebRequester _requester;
+
+        public Consumer()
+        {
+            _requester = new WebRequester();
+        }
+
+        public WeeklyPriceSerie Get(string url)
+        {
+            var returnString = _requester.GET(url);
+            var returnValues = WeeklyPriceSerie.FromJson(returnString);
+            return returnValues;
+        }
+
+        public static string urlBuilder(IUrlable url)
+        {
+            var result = $"{url.Protocol}://{url.BaseDomain}function={url.Function}";
+            result = url.Parameters.Aggregate(result, (current, parameter) => current + $"&{parameter.Key}={parameter.Value}");
+            result += $"&apikey={url.ApiKey}";
+            return result;
+        }
+    }
+}
